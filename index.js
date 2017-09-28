17da12df56e490f3f6e0143a21414a3e4205da70
Ricardo Parro <ricardo.parro@gmail.com>
2017-09-28 22:21:05 +0200

    server up and running and saving coordinates into firebase

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 var express = require('express');
-var mqtt = require('mqtt')
+var mqtt = require('mqtt');
+var admin = require("firebase-admin");
 
-var app = express();
+// Fetch the service account key JSON file contents
+var serviceAccount = require('./GalaxyWings-8a86cdaccead.json');
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://galaxy-wings-1506591701684.firebaseio.com/"
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("coordinates");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+
+/*var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -18,6 +36,7 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+*/
 
 var options = {
   port: 17737,
@@ -25,35 +44,47 @@ var options = {
   clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
   username: 'vucozfyi',
   password: 'YyemG1BMzsMf',
-  keepalive: 60,
+  keepalive: 1000,
   reconnectPeriod: 1000,
   protocolId: 'MQIsdp',
   protocolVersion: 3,
-  clean: true,
+  clean: false,
   encoding: 'utf8'
 };
 
 
 var client  = mqtt.connect('mqtt://m21.cloudmqtt.com', options);
- 
+var usersRef = ref.child("geo");
 client.on('connect', function () {
   
-  client.subscribe('GPS/#', function() {
+  client.subscribe('GPS topic', function() {
     // when a message arrives, do something with it
     client.on('message', function(topic, message, packet) {
+
+     var res = String(message).split(",");
+
+   
+      usersRef.update({
+         lat: res[0],
+         long: res[1]
+      });
+ 
+
         console.log("Received '" + message + "' on '" + topic + "'");
-    })});
-  
+    }.bind(this));
+
+    
+  });
+
   
   //client.subscribe('presence')
   //client.publish('presence', 'Hello mqtt')
 
 
 
-})
+});
  
 client.on('message', function (topic, message) {
   // message is Buffer
-  console.log(message.toString())
-  client.end()
+  console.log(message.toString());
 })
